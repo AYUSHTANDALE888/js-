6ff8feb0dbdbd9fcf71ce8840ed6dabbe7e9e1ef
Ayush Tandale <ayushtandale666@gmail.com>
2025-01-05 12:17:35 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,6 +6,26 @@ console.log(3
     +
      3)  //code readability should be high
 
-     
-console.log("AYUSH") 
+console.log("ayush")
+
+
+
+let name = "ayush"
+let age = 18
+let isLoggedin = false
+let state;
+
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//Boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null);  //object
 
