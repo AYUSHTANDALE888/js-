0f421d554b7dc8a6ac44396531b0636afabd5685
Ayush Tandale <ayushtandale666@gmail.com>
2025-01-09 14:35:56 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Ayush"
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let IsLoggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+//"Ayush" => true
+
+let someNumber = 33
+
+let stringnumeber = String(someNumber)
+console.log(stringnumeber);
+console.log(typeof stringnumeber);
+
