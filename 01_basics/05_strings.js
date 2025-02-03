d7ad012c269072c2c9f33a8b07bb2fbd0fea6841
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-03 08:45:23 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Ayush"
+const repoCount = 60
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ayushat')
