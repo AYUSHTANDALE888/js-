f7cf285adab9e68e5951f1fb4b5910978d481a68
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-04 17:49:59 +0000

    prototype

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,3 +6,13 @@ const repoCount = 60
 console.log(`Hello name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('ayushat')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('u'));
+
