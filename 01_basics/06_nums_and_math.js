c55d159f30d419158c70a595afe97e9e7628d056
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-11 17:50:56 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.6969
+
+console.log(otherNumber.toPrecision(3));  //124
+console.log(otherNumber.toPrecision(4));  //123.7
+console.log(otherNumber.toPrecision(2));  //1.2e+2
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
