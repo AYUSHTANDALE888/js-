d75d41ccc29b061a522cfa60bf312928a117c8d0
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-15 18:54:22 +0000

    array3

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -11,4 +11,14 @@ const dc_heroes = ["batman","superman","flash"]
 
 const all_New_Heroes = [...marvel_heroes , ...dc_heroes]
 
-console.log(all_New_Heroes);
+// console.log(all_New_Heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6,], 7, [6, 7,[4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Ayush"))
+console.log(Array.from("Ayush"))
+console.log(Array.from({name: "Ayush"}))
+
