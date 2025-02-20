2dfb5fb523cd7360e92387b6937858a97fe11d62
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-20 17:33:38 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+//singleton
+//object.create
+
+//object literals
+
+const JsUser = {
+    name: "Ayush",
+    age: 18,
+    location: "Nashik",
+    email: "ayush@gmail.com",
+    isLoggedIn: false,
+    lastloginDays: ["monday","saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
