667ab60dfda578241afc9a16e913618371e73869
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-21 13:20:53 +0000

    obj

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,8 +3,12 @@
 
 //object literals
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Ayush",
+    "full name": "Ayush Tandale",
+    [mySym]: "mykey1",
     age: 18,
     location: "Nashik",
     email: "ayush@gmail.com",
@@ -12,6 +16,22 @@ const JsUser = {
     lastloginDays: ["monday","saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
 
+JsUser.email = "Ayush@damn.com"
+// Object.freeze(JsUser)
+JsUser.email = "Ayush@crazy.com"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello Js User"); 
+}
+JsUser.greetingTwo = function() {
+    console.log(`Hello Js User, ${this.name}`); 
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
