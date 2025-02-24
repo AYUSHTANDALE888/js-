fa214cdcf0cff8e05e86cde6143bf0a7244f3613
Ayush Tandale <ayushtandale666@gmail.com>
2025-02-24 18:10:30 +0000

    complete

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,8 +27,43 @@ const result = addTwoNumber(3, 5)
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username){
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Ayush"))
+// console.log(loginUserMessage("Ayush"))
+// console.log(loginUserMessage("Ayush"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Ayush",
+    prices: 999
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
